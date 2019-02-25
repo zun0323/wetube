603b6460ac11b6698a12698a3c596e59a8d85505
Ryan <zun0323@gmail.com>
2019-02-25 21:40:49 +0900

    setting babel for PM2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
@@ -7,11 +7,17 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost: ${PORT}`);
 };
 
-const handleMain = (req, res) => {
+const handleHome = (req, res) => {
   console.log(req);
   res.send("This is main page");
 };
 
-app.get(`/`, handleMain);
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+};
+
+app.get(`/`, handleHome);
+
+app.get(`/profile`, handleProfile);
 
 app.listen(PORT, handleListening);

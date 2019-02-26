19d6df3c3c5b79382291ba5c096441cd5c695dce
Ryan <zun0323@gmail.com>
2019-02-26 21:52:56 +0900

    small changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
@@ -9,21 +11,16 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost: ${PORT}`);
 };
 
-const handleHome = (req, res) => {
-  res.send("This is main page");
-};
+const handleHome = (req, res) => res.send("This is main page");
 
-const handleProfile = (req, res) => {
-  res.send("You are on my profile");
-};
+const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-};
-
-app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(morgan("dev"));
+
 app.get(`/`, handleHome);
 app.get(`/profile`, handleProfile);
 app.listen(PORT, handleListening);

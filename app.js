3e8e258e8556898b3f7e828021fd796c84773e4d
Ryan <zun0323@gmail.com>
2019-02-26 23:50:50 +0900

    install nodemon

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const handleHome = (req, res) => res.send("This is main page");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get(`/`, handleHome);
+app.get(`/profile`, handleProfile);

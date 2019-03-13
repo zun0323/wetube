c38745b373df204e66d02b5c77a8fe39640fca3f
Ryan <zun0323@gmail.com>
2019-03-13 16:37:09 +0900

    #3.5 Home Controller Finished

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const CommentSchema = new mongoose.Schema({
   text: {
     type: String,
-    require: "Text is required"
+    require: "Text is needed"
   },
   createdAt: {
     type: Date,
